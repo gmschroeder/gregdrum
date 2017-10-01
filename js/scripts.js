5a333705c29581172bdbe03b43cd99d6ece75f6c
Greg Schroeder <gmschroeder@gmail.com>
2017-10-01 18:47:46 +0800

    Initial commit. Sawtooth with fixed amplitude envelope with pitch chosen from pentatonic scale. vertical placement of tap changes pitch.
    zoom is disabled, screen is blank
    
    Amp envelope needs to be controllable, ugly conditional needs to be rethought.
    Needs more 'expressiveness' in general

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+var ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+var tapper = document.getElementById("strike");
+
+var now;
+var osc;
+var gainNode;
+
+tapper.addEventListener('click', function( evt ) {
+    osc = ctx.createOscillator();
+    osc.type = 'sawtooth';
+
+
+    var scale = {
+        'c': 261,
+        'd': 293.665,
+        'e': 329.628,
+        'a': 440,
+        'c2': 523.251
+    }
+
+    var keySize = window.innerHeight / 5;
+
+
+    if (evt.screenY < keySize ) {
+        osc.frequency.value = scale.c;
+    } else if (evt.screenY > keySize && evt.screenY < keySize * 2) {
+        osc.frequency.value = scale.d;
+    } else if (evt.screenY > keySize * 2 && evt.screenY < keySize * 3) {
+        osc.frequency.value = scale.e;
+    } else if (evt.screenY > keySize * 3 && evt.screenY < keySize * 4) {
+        osc.frequency.value = scale.a;
+    } else {
+        osc.frequency.value = scale.c2;
+
+    }
+    gainNode = ctx.createGain();
+    gainNode.gain.value = 0;
+
+    
+
+    osc.connect(gainNode);
+    gainNode.connect(ctx.destination);
+    osc.start();
+
+    now = ctx.currentTime;
+    gainNode.gain.exponentialRampToValueAtTime(1, now+0.00001);
+    gainNode.gain.exponentialRampToValueAtTime(.000000000001,now + 3);
+});
+
---
+++ js/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+// draw 5 circles and assign names
+    createCanvas(innerWidth, innerHeight);
+    background(0);
+    noStroke();
+
+    fill(255);
+    ellipse(innerWidth/2, innerHeight/2, 180, 180);
+
+    }
+
+function draw() {
+}
