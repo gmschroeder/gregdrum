66d487bb8354bba7e44502355456cd9844cad189
Greg <gmschroeder@gmail.com>
2017-11-28 15:02:48 +0800

    removed location-based pitch, began responsive gui layout

--- js/scripts.js
+++ js/scripts.js
@@ -7,45 +7,6 @@ var osc;
 var gainNode;
 
 tapper.addEventListener('click', function( evt ) {
-    osc = ctx.createOscillator();
-    osc.type = 'sawtooth';
-
-
-    var scale = {
-        'c2': 523.251,
-        'a': 440,
-        'g': 392,
-        'e': 329.628,
-        'd': 293.665,
-        'c': 261
-    }
-
-    var keySize = window.innerHeight / 6;
-
-    console.log("window height");
-    console.log(window.innerHeight);
-    console.log("this click:");
-    console.log(evt.clientY)
-
-    if (evt.clientY < keySize ) {
-        osc.frequency.value = scale.c2;
-    } else if (evt.clientY > keySize && evt.clientY < keySize * 2) {
-        osc.frequency.value = scale.a;
-
-    } else if (evt.clientY > keySize * 2 && evt.clientY < keySize * 3) {
-        osc.frequency.value = scale.g;
-
-    } else if (evt.clientY > keySize * 3 && evt.clientY < keySize * 4) {
-        osc.frequency.value = scale.e;
-
-    } else if (evt.clientY > keySize * 4 && evt.clientY < keySize * 5) {
-        osc.frequency.value = scale.d;
-
-    } else if (evt.clientY > keySize * 5) {
-        osc.frequency.value = scale.c;
-
-    }
-
     console.log(osc.frequency.value);
 
 
